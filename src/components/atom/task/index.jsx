fb7c15834d938808eafc1feb9c657a9e8470b4ec
stefanstefanovic94 <stefan.stefanovic@kortechs.io>
2021-01-22 11:57:55 +0100

    done

--- src/components/atom/task/index.jsx
+++ src/components/atom/task/index.jsx
@@ -1,37 +1,33 @@
-import React, { useState } from "react"
-import DeleteButton from "../deleteButton/deteleButton"
-import "./task.css"
-import PropTypes from "prop-types"
-import CheckBox from "../checkBox"
+import React, { useState } from "react";
+import DeleteButton from "../deleteButton/deteleButton";
+import "./task.css";
+import PropTypes from "prop-types";
+import CheckBox from "../checkBox";
 
 const Task = ({ task, id, deleteItem, isActive, change }) => {
-
-
-    const deleteWraper = () => {
-        deleteItem(id)
-    }
-    // const [check, setCheck] = useState(false);
-
-    const checkBox = () => {
-        const checked = !isActive
-        // setCheck(checked)
-        change(id)
-    }
-    return (
-        <li className={!isActive ? "listItem" : "listItemColor"}>
-            {task}
-            <div className="wrap">
-                <CheckBox state={isActive} check={checkBox} />
-                <DeleteButton onClick={deleteWraper} />
-            </div>
-        </li>
-
-    )
-}
+  const deleteWraper = () => {
+    deleteItem(id);
+  };
+  // const [check, setCheck] = useState(false);
+  const checked = isActive;
+  const checkBox = () => {
+    // setCheck(checked)
+    change(id);
+  };
+  return (
+    <li className={!isActive ? "listItem" : "listItemColor"}>
+      {task}
+      <div className="wrap">
+        <CheckBox state={checked} check={checkBox} />
+        <DeleteButton onClick={deleteWraper} />
+      </div>
+    </li>
+  );
+};
 
 Task.propTypes = {
-    task: PropTypes.string,
-    id: PropTypes.number,
-    deleteItem: PropTypes.func
-}
-export default Task
+  task: PropTypes.string,
+  id: PropTypes.number,
+  deleteItem: PropTypes.func,
+};
+export default Task;
--- src/components/pages/toDo/index.jsx
+++ src/components/pages/toDo/index.jsx
@@ -9,19 +9,22 @@ const ToDo = () => {
     const [taskList, setTaskList] = useState([]);
     console.log("ToDo -> taskList", taskList)
     const [filter, setFilter] = useState("")
-
+    
     const createTodo = (textInput) => {
         const lista = [...taskList, { name: textInput, isActive: false }]
         setTaskList(lista)
     }
-
+    
     const deleteItem = (index) => {
-        const newArr = [...taskList]
+        const newArr = [...filteredList]
         newArr.splice(index, 1)
         setTaskList(newArr)
     }
+    const filteredList = taskList.filter((task) => {
+        return task.name.toLowerCase().includes(filter.toLowerCase())
+    })
     const change = (id) => {
-        setTaskList(taskList.map((list, index) => {
+        setTaskList(filteredList.map((list, index) => {
             if (id === index) {
                 return {
                     ...list,
@@ -37,16 +40,13 @@ const ToDo = () => {
         setFilter(text)
     }
 
-    const filteredList = taskList.filter((task) => {
-        return task.name.toLowerCase().includes(filter.toLowerCase())
-    })
 
     return (
         <div className='wraper'>
             <ToDoHeader count={filteredList} />
-            <SearchPage searchText={searchText} />
-            <TaskList taskList={filteredList} deleteItem={deleteItem} change={change} />
             <ToDoFooter createTodo={createTodo} />
+            <TaskList taskList={filteredList} deleteItem={deleteItem} change={change} />
+            <SearchPage searchText={searchText} />
         </div>
     )
 }
